3b649ec0cc4a18ef04db3a9ea7e32b97dc95b4c1
Navneet Kamboj <148902434+navneet228@users.noreply.github.com>
2024-12-05 20:55:26 -0500

    commit

---
+++ cart.js
@@ -0,0 +1,32 @@
+var Cart_btn= document.getElementById('Cartlrt1');
+Cart_btn.addEventListener('click',function(){
+alert("Product is add into cart")
+});
+var Cart_btn= document.getElementById('Cartlrt2');
+Cart_btn.addEventListener('click',function(){
+alert("Product is add into cart")
+});
+var Cart_btn= document.getElementById('Cartlrt3');
+Cart_btn.addEventListener('click',function(){
+alert("Product is add into cart")
+});
+var Cart_btn= document.getElementById('Cartlrt4');
+Cart_btn.addEventListener('click',function(){
+alert("Product is add into cart")
+});
+var Cart_btn= document.getElementById('Cartlrt5');
+Cart_btn.addEventListener('click',function(){
+alert("Product is add into cart")
+});
+var Cart_btn= document.getElementById('Cartlrt6');
+Cart_btn.addEventListener('click',function(){
+alert("Product is add into cart")
+});
+var Cart_btn= document.getElementById('Cartlrt7');
+Cart_btn.addEventListener('click',function(){
+alert("Product is add into cart")
+});
+var Cart_btn= document.getElementById('Cartlrt8');
+Cart_btn.addEventListener('click',function(){
+alert("Product is add into cart")
+});
